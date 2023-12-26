a853d8ee4b812b17c366e5c6d29d42873096752e
jf7689 <jf7689@rit.edu>
2023-12-26 17:51:43 -0500

    Change menu icon between open and close

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     navToggle.addEventListener("click", () => {
         primaryNav.toggleAttribute("data-visible");
+        primaryNav.hasAttribute("data-visible")
+        ? navToggle.children[0].src = "assets/images/closeIcon.png"
+        : navToggle.children[0].src = "assets/images/menuIcon.png";
     });
 
 });
